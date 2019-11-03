50e22f37244b31a33871baf4831ccddb1d3dcecc
Peter Cole <petercole3@gmail.com>
2019-11-03 01:05:41 -0700

    Question 1 complete

--- tests.js
+++ tests.js
@@ -2,7 +2,7 @@
 
 // You should not modify anything in this file, but feel free to look through it!
 
-function testSum() { //eslint-disable-line
+function testSum() {
   if (sum(4,7)[0] === 11 && sum(4,7)[1] === 'The sum of 4 and 7 is 11.') {
     console.log('%c TEST FOR sum() PASSES', 'color: green');
   } else {
